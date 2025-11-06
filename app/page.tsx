31b28fbaebc3f81cb7de1854b36595370719ad76
Gustavo Martins <gustavosm994@gmail.com>
2025-11-06 19:42:44 -0300

    website

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,104 @@
 import Image from "next/image";
+import { InstagramIcon } from "./components/instagram";
+import { ThreadsIcon } from "./components/threads";
+import { TiktokIcon } from "./components/tiktok";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <>
+      <main className="min-h-screen bg-[url(/background.png)] bg-no-repeat bg-cover bg-center bg-fixed flex flex-col items-center p-4 space-y-4 xl:space-y-5">
+        <div className="size-44 relative">
+          <Image
+            src="/avatar.jpg"
+            alt="Foto de perfil"
+            fill
+            className="object-cover rounded-full"
+          />
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+        <h1 className="text-4xl font-bold md:text-5xl italic">Laura Barreto</h1>
+        <h2 className="font-sans text-base md:text-2xl text-center"><strong>Estrategista digital</strong> especialista em reputação e posicionamento</h2>
+
+        <section className="container mx-auto flex flex-col gap-4 xl:grid xl:grid-cols-2">
+          <div className="flex h-40 flex-col justify-between py-2 px-4 bg-white/10 rounded-2xl shadow-md xl:h-56 xl:py-4">
+            <h3 className="font-serif text-xl md:text-2xl italic font-bold xl:text-3xl">Consultoria estratégica</h3>
+            <span className="font-sans text-sm md:text-base w-full xl:text-lg">Em 2 meses te ensino a se posicionar, construir sua reputação digital e<br /> <strong>vender mais com estratégia.</strong></span>
+            <button className="font-sans bg-pink-500 rounded-2xl text-sm md:text-base text-neutral-50 font-medium p-1 md:p-2 xl:text-lg"><strong>Quero acessar</strong> a consultoria</button>
+          </div>
+
+          <div className="flex h-40 flex-col justify-between py-2 px-4 bg-white/10 rounded-2xl shadow-md xl:h-56 xl:py-4">
+            <h3 className="font-serif text-xl md:text-2xl italic font-bold xl:text-3xl">Serviços de design & estratégia</h3>
+            <span className="font-sans text-sm md:text-base w-full xl:text-lg">Construa a reputação digital do seu negócio com a estratégia e comunicação certa.</span>
+            <button className="font-sans bg-pink-500 rounded-2xl text-sm md:text-base text-neutral-50 font-medium p-1 md:p-2 xl:text-lg"><strong>Quero acessar</strong> a consultoria</button>
+          </div>
+
+          <div className="flex h-40 flex-col justify-between py-2 px-4 bg-white/10 rounded-2xl shadow-md xl:h-56 xl:py-4">
+            <h3 className="font-serif text-xl md:text-2xl italic font-bold xl:text-3xl">Storymaker</h3>
+            <span className="font-sans text-sm md:text-base w-full xl:text-lg">Produção de vídeos mobile para eventos como casamentos, aniversários, chás de bebê e eventos corporativos.</span>
+            <button className="font-sans bg-pink-500 rounded-2xl text-sm md:text-base text-neutral-50 font-medium p-1 md:p-2 xl:text-lg"><strong>Quero acessar</strong> a consultoria</button>
+          </div>
+
+          <div className="flex h-40 flex-col justify-center py-2 px-4 bg-white/10 rounded-2xl shadow-md xl:h-56 xl:py-4">
+            <h3 className="font-serif text-4xl text-center italic font-bold underline decoration-3 underline-offset-3 decoration-pink-500">Fale comigo!</h3>
+          </div>
+        </section>
+
+        <h4 className="font-sans font-medium text-lg md:text-2xl">Me acompanhe nas redes sociais</h4>
+
+        <section className="container mx-auto grid grid-cols-2 justify-center gap-4">
+          <div className="flex col-span-2 items-center justify-center gap-2 bg-white/10 rounded-2xl shadow-md p-2">
+            <InstagramIcon />
+            <span className="text-lg md:text-2xl">laurabarretomkt</span>
+          </div>
+
+          <div className="flex items-center justify-center gap-2 bg-white/10 rounded-2xl shadow-md p-2">
+            <ThreadsIcon />
+            <span className="text-lg md:text-2xl">laurabarretomkt</span>
+          </div>
+
+          <div className="flex items-center justify-center gap-2 bg-white/10 rounded-2xl shadow-md p-2">
+            <TiktokIcon />
+            <span className="text-lg md:text-2xl">laurabarretomkt</span>
+          </div>
+        </section>
       </main>
-    </div>
+
+      <footer className="relative w-full bg-neutral-50 text-sm font-sans p-4">
+        <div className="container mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-3 space-y-4 gap-4 xl:text-lg">
+            <div className="flex flex-col space-y-2">
+              <p className="text-neutral-950 font-serif italic font-bold text-2xl">
+                Laura Barreto
+              </p>
+              <p className="text-neutral-950">
+                Estrategista digital especialista em reputação e posicionamento
+              </p>
+            </div>
+
+            <div className="flex flex-col space-y-2">
+              <p className="text-neutral-950">
+                Canais de atendimento
+              </p>
+              <p className="text-neutral-950">
+                (Informações, dúvidas e orçamentos)
+              </p>
+              <p className="text-neutral-950">
+                lauramachado039@gmail.com
+              </p>
+              <p className="text-neutral-950">+55 (48) 99196-5786</p>
+            </div>
+
+            <div className="flex flex-col space-y-2">
+              <p className="text-neutral-950">
+                Serviços
+              </p>
+              <p className="text-neutral-950">Gerenciamento de mídias sociais</p>
+              <p className="text-neutral-950">Desenvolvimento de website</p>
+              <p className="text-neutral-950">Criação de identidade visual</p>
+              <p className="text-neutral-950">Gestão de tráfego pago</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
   );
 }
